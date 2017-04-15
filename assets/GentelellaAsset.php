066c46e5b1113c3e2bdd40444c8bba11dd2b51df
liumei <liumeishengqi@huluwa.cc>
2017-04-15 10:33:09 +0800

    将bower库移出

--- assets/GentelellaAsset.php
+++ assets/GentelellaAsset.php
@@ -7,25 +7,29 @@
  * Date: 4/14/17
  * Time: 4:33 PM
  */
+
 namespace liumapp\theme\assets;
 
 use yii\web\AssetBundle;
 
-/**
- * Main backend application asset bundle.
- */
 class GentelellaAsset extends AssetBundle
 {
-    public $basePath = __DIR__ . '/../bower_components/build';
+
+    public $basePath = '@vendor/liumapp/theme/bower_components/build';
+
     public $baseUrl = '@web';
+
     public $css = [
         'css/custom.css',
     ];
+
     public $js = [
         'js/custom.js',
     ];
+
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
 }
